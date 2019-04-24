da20bfba7b2f142d38a54a9a26f2a7297e44ed5e
brendan sudol <brendansudol@gmail.com>
2019-04-24 01:13:27 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d204ff8795cff376ad426ad528717d3.js"
+  "/precache-manifest.6fd37159bdfcf29b8ad1b533796a5eb7.js"
 );
 
 self.addEventListener('message', (event) => {
